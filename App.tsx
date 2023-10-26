9999899eeefa54761461f22cce15f4952751e233
ba2nOshriyagit config --global user.email shriyasingh1100@gmail.com <137395944+ba2n0shriya@users.noreply.github.com>
2023-10-26 09:45:14 +0530

    26-10-2023mrng

--- App.tsx
+++ App.tsx
@@ -116,10 +116,10 @@ useEffect(() => {
         const newTimer = prevTimer - 1;
         
         // Check if the timer has reached 86300 seconds (23 hours and 58 minutes)
-        if (newTimer <= 46300) {
+        if (newTimer <= 86300) {
           openLogoutPopup(); // Open the modal
         
-        } if (newTimer <= 46200) {
+        } if (newTimer <= 86200) {
           clearInterval(unique); // Stop the interval
           setIsLoading(true); // Show loading indicator
           
